4589e5c93112f94bac45abf71e199c4f07de75cd
Sunny <swarnendughosh05@gmail.com>
2024-10-06 23:07:39 +0530

    cleane up

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Button, Text, View } from "react-native";
+
+export default function() {
+    return <View>
+        <Text>Hi there</Text>
+        <Button title="Signin"></Button>
+    </View>
+}
