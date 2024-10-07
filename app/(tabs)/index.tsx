dc6fdd784ae2401dbc2b833f97f12d0c94db4656
Sunny <swarnendughosh05@gmail.com>
2024-10-07 20:48:41 +0530

    nested navbae added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,30 @@
-import {  Text, View, ViewComponent } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {  createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { View , Text} from "react-native";
+
+
+const Tab = createMaterialTopTabNavigator();
 
 export  default function forYou() {
-    return <SafeAreaView>
-        <Text>
-            For you Page
-        </Text>
-    </SafeAreaView>
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={homoscreen}/>
+            <Tab.Screen name="Setting" component={SettingsScreen}/>
+        </Tab.Navigator>
+    )
+}
+
+function homoscreen() {
+    return <View>
+          <Text>
+             hi home 
+          </Text>
+    </View>
+}
+function SettingsScreen() {
+    return <View>
+          <Text>
+             hi  setting
+          </Text>
+    </View>
 }
---
+++ app/suggested.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Libray() {
+    return (
+        <View>
+            <Text>
+                Suggested
+            </Text>
+        </View>
+    )
+}
