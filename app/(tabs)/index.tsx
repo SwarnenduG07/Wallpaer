c4b8189a3fb957d129eff97a39684badddecb85f
Sunny <swarnendughosh05@gmail.com>
2024-10-08 10:04:57 +0530

    tab navigation added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,23 +8,33 @@ const Tab = createMaterialTopTabNavigator();
 export  default function forYou() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={homoscreen}/>
-            <Tab.Screen name="Setting" component={SettingsScreen}/>
+            <Tab.Screen name="Library" component={Library}/>
+            <Tab.Screen name="Liked" component={Liked}/>
+            <Tab.Screen name="Suggested" component={Suggested} />
         </Tab.Navigator>
     )
 }
 
-function homoscreen() {
+function Library() {
     return <View>
-          <Text>
-             hi home 
-          </Text>
-    </View>
+              <Text>
+                 hi From  Library
+              </Text>
+          </View>
 }
-function SettingsScreen() {
+
+function Liked() {
     return <View>
-          <Text>
-             hi  setting
-          </Text>
-    </View>
+             <Text>
+                hi Liked Page
+             </Text>
+          </View>
+}
+
+function Suggested() {
+    return <View>
+              <Text>
+                hi Suggested Page
+             </Text>
+          </View>
 }
