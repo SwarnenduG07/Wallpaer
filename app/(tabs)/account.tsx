1ff477b59dbca6df0f8de3d6ba35d6edc5a61311
Sunny <swarnendughosh05@gmail.com>
2024-10-23 13:29:47 +0530

    account age them added

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,13 +1,15 @@
 import { DownloadPicture } from "@/components/ButtomSheet";
+import { ThemedText } from "@/components/ThemedText";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Link } from "expo-router";
 import { useState } from "react";
-import {  Button, Pressable, Text, useColorScheme, View, ViewComponent } from "react-native";
+import {  StyleSheet, Pressable, Text, useColorScheme, View, ViewComponent } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export  default function account() {
     const [pictureOpen, setPictureOpen] = useState(false)
     return <SafeAreaView style={{flex: 1}}>
+        <Header />
         <View style={{flex:1}}>
           <AuthButton
           lable="Signin"
@@ -19,12 +21,37 @@ export  default function account() {
           icon={<Ionicons name="logo-google" 
           size={22} color="white" />} 
           />
+        <ThemeSelector />
         </View>
     </SafeAreaView>
 } 
 
+function ThemeSelector() {
+        return <View style={styles.margin}>
+                <Text style={styles.textBig}>Settings</Text>
+                <Text style={styles.textSmall}>Theme</Text>
+                <View style={styles.textSmall}>
+                   <ThemeButton selected={false} title="Dark" />
+                   <ThemeButton selected={false} title="Dark" />
+                   <ThemeButton selected={false} title="system" />
+                </View>
+            </View>
+}
+
+function ThemeButton({selected, title}: {selected: boolean, title: string}) { 
+ return <Pressable>
+       <ThemedText>{title}</ThemedText>
+ </Pressable>
+}
+
+function Header() {
+    return <View style={styles.topbar}>
+        <Text style={styles.textBig}>Panels</Text>
+        <Text style={styles.textSmall}>Signin to save your data</Text>
+    </View>
+}
+
 function AuthButton({lable, icon}: {lable: string, icon: any}) {
-    const theme = useColorScheme() ?? 'light'
   
       return <Pressable style={{
            backgroundColor: "black" ,
@@ -43,4 +70,28 @@ function AuthButton({lable, icon}: {lable: string, icon: any}) {
        }}>{lable}</Text>
       </Pressable>
   }
+
+  const styles = StyleSheet.create({
+    textBig: {
+        fontSize: 30,
+        fontWeight: "semibold",
+        marginLeft: 20,
+    },
+    textSmall: {
+        fontSize: 16,
+        marginLeft: 20,
+    },
+    topbar: {
+        paddingTop: 20,
+    },
+    themselectorContainer: {
+         flex: 1,
+    },
+    themselectorChild: {
+        flex: 0.33
+    },
+    margin: {
+        marginTop: 20,
+    }
+  })
   
